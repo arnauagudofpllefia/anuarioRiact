7ad6a1845be4cefa1cf6e7cec9f585fc0298f692
arnauagudofpllefia <arnagudo05@gmail.com>
2026-01-13 20:56:20 +0100

    .

--- src/componentes/InfoAdmin.jsx
+++ src/componentes/InfoAdmin.jsx
@@ -1,8 +1,8 @@
 export const InfoAdmin = ({ user, onLogout }) => {
   return (
-    <div className="flex justify-between items-center bg-gray-100 p-2 mb-4">
+    <div className=" justify-center items-center p-2 mb-4">
       <h2>Conectado como {user?.name}</h2>
-      <button className="bg-red-500 text-white px-3 py-1 rounded" onClick={onLogout}>Logout</button>
+      <button className="bg-red-500 text-white px-3 py-1 rounded " onClick={onLogout}>Logout</button>
     </div>
   )
 }
